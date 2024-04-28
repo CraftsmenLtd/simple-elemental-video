eee1b7cba9d6a6077533115a9284589040be6a45
melon <m.melon@m2amedia.tv>
2024-04-28 20:11:53 +0600

    terraform format

--- terraform/modules.tf
+++ terraform/modules.tf
@@ -25,12 +25,12 @@ module "api-gateway" {
 }
 
 module "harvest" {
-  source                  = "./harvest"
-  prefix                  = var.prefix
-  api_gw_rest_api_id      = module.api-gateway.api_gw_rest_api_id
-  api_gw_root_resource_id = module.api-gateway.api_gw_root_resource_id
-  medialive_channel_id    = module.medialive.channel_id
-  mediapackage_channel_id = module.mediapackage.channel_id
+  source                    = "./harvest"
+  prefix                    = var.prefix
+  api_gw_rest_api_id        = module.api-gateway.api_gw_rest_api_id
+  api_gw_root_resource_id   = module.api-gateway.api_gw_root_resource_id
+  medialive_channel_id      = module.medialive.channel_id
+  mediapackage_channel_id   = module.mediapackage.channel_id
   mediapackage_hls_endpoint = module.mediapackage.hls_origin_endpoint
 }
 
