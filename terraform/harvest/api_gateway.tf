c5090c4d59aa2ebb0866819cb19e2d8490a337b5
melon <m.melon@m2amedia.tv>
2024-04-28 20:05:09 +0600

    api endpoints

--- terraform/harvest/api_gateway.tf
+++ terraform/harvest/api_gateway.tf
@@ -1,21 +1,107 @@
-# l2v
-resource "aws_api_gateway_resource" "l2v_api_resource" {
+# live
+resource "aws_api_gateway_resource" "live_api_resource" {
   rest_api_id = var.api_gw_rest_api_id
   parent_id   = var.api_gw_root_resource_id
-  path_part   = "l2v"
+  path_part   = "live"
 }
 
-# l2v/live/manifest
+# live/manifest
 module "api_live_manifest" {
   source = "./../commons/api_gateway"
 
-  api_path = "live"
+  api_path = "manifest"
+  methods  = ["GET"]
+
+  region        = data.aws_region.current.name
+  prefix        = var.prefix
+  rest_api_id   = var.api_gw_rest_api_id
+  parent_api_id = aws_api_gateway_resource.live_api_resource.id
+
+  api_key_required = false
+
+  enable_lambda_integration = true
+  lambda_function_arn       = aws_lambda_function.lambda_functions[local.lambda_options.l2v-harvest.name].arn
+}
+
+# live/marker
+module "api_live_manifest" {
+  source = "./../commons/api_gateway"
+
+  api_path = "manifest"
   methods  = ["POST"]
 
   region        = data.aws_region.current.name
   prefix        = var.prefix
   rest_api_id   = var.api_gw_rest_api_id
-  parent_api_id = aws_api_gateway_resource.l2v_api_resource.id
+  parent_api_id = aws_api_gateway_resource.live_api_resource.id
+
+  api_key_required = false
+
+  enable_lambda_integration = true
+  lambda_function_arn       = aws_lambda_function.lambda_functions[local.lambda_options.l2v-harvest.name].arn
+}
+
+# harvest
+resource "aws_api_gateway_resource" "harvest_api_resource" {
+  rest_api_id = var.api_gw_rest_api_id
+  parent_id   = var.api_gw_root_resource_id
+  path_part   = "harvest"
+}
+
+# harvest/job
+module "api_live_manifest" {
+  source = "./../commons/api_gateway"
+
+  api_path = "job"
+  methods  = ["POST"]
+
+  region        = data.aws_region.current.name
+  prefix        = var.prefix
+  rest_api_id   = var.api_gw_rest_api_id
+  parent_api_id = aws_api_gateway_resource.harvest_api_resource.id
+
+  api_key_required = false
+
+  enable_lambda_integration = true
+  lambda_function_arn       = aws_lambda_function.lambda_functions[local.lambda_options.l2v-harvest.name].arn
+}
+
+# harvest/status
+module "api_live_manifest" {
+  source = "./../commons/api_gateway"
+
+  api_path = "status"
+  methods  = ["GET"]
+
+  region        = data.aws_region.current.name
+  prefix        = var.prefix
+  rest_api_id   = var.api_gw_rest_api_id
+  parent_api_id = aws_api_gateway_resource.harvest_api_resource.id
+
+  api_key_required = false
+
+  enable_lambda_integration = true
+  lambda_function_arn       = aws_lambda_function.lambda_functions[local.lambda_options.l2v-harvest.name].arn
+}
+
+# vod
+resource "aws_api_gateway_resource" "vod_api_resource" {
+  rest_api_id = var.api_gw_rest_api_id
+  parent_id   = var.api_gw_root_resource_id
+  path_part   = "vod"
+}
+
+# vod/manifest
+module "api_live_manifest" {
+  source = "./../commons/api_gateway"
+
+  api_path = "manifest"
+  methods  = ["POST"]
+
+  region        = data.aws_region.current.name
+  prefix        = var.prefix
+  rest_api_id   = var.api_gw_rest_api_id
+  parent_api_id = aws_api_gateway_resource.vod_api_resource.id
 
   api_key_required = false
 
