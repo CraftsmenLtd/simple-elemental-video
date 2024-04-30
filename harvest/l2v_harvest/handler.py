86752d479e9b56e851271b21850a47d08d5cbaa5
saimon <saimoncse19@gmail.com>
2024-04-30 15:11:09 +0600

    feat: Add an endpoint show the status of the harvest job

--- harvest/l2v_harvest/handler.py
+++ harvest/l2v_harvest/handler.py
@@ -11,6 +11,7 @@ import logging
 from http import HTTPStatus
 
 from .job_create import handle_create_harvest_job
+from .job_status import handle_get_harvest_job_status
 from .utils import create_response
 
 LOGGER = logging.getLogger()
@@ -35,8 +36,11 @@ def handler(event, context):
     LOGGER.info("Received %s request for %s", http_method, path)
 
     try:
-        if path in ["/harvest-jobs", "/harvest-jobs/"] and http_method == "POST":
+        if path in ["/harvest/jobs", "/harvest/jobs/"] and http_method == "POST":
             return handle_create_harvest_job(event)
+        if path.startswith("/harvest/jobs/") and http_method == "GET":
+            job_id = path.split("/")[-1]
+            return handle_get_harvest_job_status(job_id)
     # for unforeseeable scenarios
     except Exception as error:
         LOGGER.exception("Error processing request.", exc_info=error)
---
+++ harvest/l2v_harvest/job_status.py
@@ -0,0 +1,30 @@
+from http import HTTPStatus
+
+import boto3
+
+from .utils import create_response
+
+
+def handle_get_harvest_job_status(job_id: str) -> dict:
+    """Retrieve and return the status of a MediaPackage harvest job.
+
+    Parameters:
+    - job_id (str): The unique identifier for the harvest job.
+
+    Returns:
+    - dict: status of the harvest job
+    """
+    mediapackage_client = boto3.client("mediapackage")
+
+    try:
+        response = mediapackage_client.describe_harvest_job(Id=job_id)
+        status = response.get('Status')
+        return create_response(
+            status_code=HTTPStatus.OK,
+            body={"jobId": job_id, "status": status}
+        )
+    except mediapackage_client.exceptions.ClientError as e:
+        return create_response(
+            status_code=HTTPStatus.INTERNAL_SERVER_ERROR,
+            body={"error": str(e)}
+        )
