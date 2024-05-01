3738a529c4965bf5760feef30178cc9ffda2cf9c
Melon <m.melon@m2amedia.tv>
2024-05-01 12:46:07 +0600

    Fix import python

--- harvest/l2v_harvest/manifest_handler.py
+++ harvest/l2v_harvest/manifest_handler.py
@@ -1,5 +1,11 @@
+from http import HTTPStatus
+
+from utils import create_response
 from lambda_env import LambdaEnv
 
 
 def handle_get_manifest(event, lambda_environment: LambdaEnv):
-    return lambda_environment.mediapackage_hls_endpoint
+    return create_response(
+        status_code=HTTPStatus.OK,
+        body={"endpoint": lambda_environment.mediapackage_hls_endpoint}
+    )
